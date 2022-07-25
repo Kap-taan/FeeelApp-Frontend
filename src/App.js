8d10f5ac5783844daca5753a0670f24a1b490162
user <harshsukhijacse@email.com>
2022-07-25 13:41:09 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import Loading from "./components/general/Loading";
+import MainPage from './components/general/MainPage';
+import Index from "./components/user";
+import { AuthProvider } from './stores/AuthContext';
+import RequireAuth from "./routes/PrivateRoute";
+import PublicAuth from "./routes/PublicRoute";
+import UserProfile from './components/user/UserProfile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path='/' element={<MainPage />} /> */}
+            {/* <Route path="/dashboard" element={<Index />} /> */}
+            <Route
+              path="/"
+              element={
+                <PublicAuth>
+                  <MainPage />
+                </PublicAuth>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <RequireAuth>
+                  <Index />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/user/:userName/:isFollowed"
+              element={
+                <RequireAuth>
+                  <UserProfile />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicAuth>
+                  <Login />
+                </PublicAuth>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <PublicAuth>
+                  <Signup />
+                </PublicAuth>
+              }
+            />
+            {/* <Route path='/login' element={<Login />} /> */}
+            {/* <Route path='/signup' element={<Signup />} /> */}
+          </Routes>
+        </BrowserRouter>
+        {/* <Loading /> */}
+      </AuthProvider>
     </div>
   );
 }
